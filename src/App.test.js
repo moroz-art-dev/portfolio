d16e0e6798a52138809ccb6c414813ce9f29ee80
moroz-art-dev <moroz.art.dev@gmail.com>
2023-06-22 15:27:19 +0300

    fix eslint

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,7 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { test, expect } from '@jest/globals';
 
 test('renders learn react link', () => {
   render(<App />);
