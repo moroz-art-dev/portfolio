eb5cf10e9e0a6f8cfbed61f47fa6d242ac20bdbd
moroz-art-dev <moroz.art.dev@gmail.com>
2023-07-01 22:33:55 +0300

    update projects

--- src/data/projects.js
+++ src/data/projects.js
@@ -2,6 +2,7 @@ import photo1 from '../assets/little_lemon_1.png';
 import photo2 from '../assets/little_lemon_2.png';
 import photo3 from '../assets/little_lemon_3.png';
 import photo4 from '../assets/little_lemon_4.png';
+import photo5 from '../assets/live-search.png';
 
 const projects = {
   title: 'Featured Projects',
@@ -12,6 +13,13 @@ const projects = {
         'The project includes a landing page and reservation form for a restaurant, providing essential information and enabling easy table bookings.',
       imagesSrc: [() => photo1, () => photo2, () => photo3, () => photo4],
       url: 'https://github.com/moroz-art-dev/little-lemon',
+    },
+    {
+      title: 'Live Search',
+      description:
+        'The project  is a live search application built with Next.js 13, TypeScript, MUI, and Redux Toolkit.',
+      imagesSrc: [() => photo5],
+      url: 'https://live-search-kappa.vercel.app',
     },
   ],
 };
